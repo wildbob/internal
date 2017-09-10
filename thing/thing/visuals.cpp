a67740d20440d961ef7f7b8082bc9bcce5845b1c
wildbob <30989313+wildbob@users.noreply.github.com>
2017-09-09 22:36:55 -0700

    proper getbox func
    arrays weren't working for me so you'll see a lot of shitty code

--- thing/thing/visuals.cpp
+++ thing/thing/visuals.cpp
@@ -16,27 +16,64 @@ c_visuals::~c_visuals()
 
 auto c_visuals::get_box(c_entity* entity) -> b_box
 {
-	b_box box;
-	Vector m_vecFoot = entity->get_origin();
-	Vector m_vecHead;
+		b_box box{};
 
-	if (entity->get_flags() & FL_DUCKING)
-		m_vecHead = m_vecFoot + Vector(0.f, 0.f, 52.f);
-	else
-		m_vecHead = m_vecFoot + Vector(0.f, 0.f, 72.f);
+	Vector flb, brt, blb, frt, frb, brb, blt, flt;
 
-	Vector m_vecFootScreen, m_vecHeadScreen;
+	Vector m_min = entity->get_min();
+	Vector m_max = entity->get_max();
+	const matrix3x4_t& trans = entity->get_coordinate_frame();
 
-	if (g_drawings.world_to_screen(m_vecFoot, m_vecFootScreen) && g_drawings.world_to_screen(m_vecHead, m_vecHeadScreen))
-	{
-		box.h = int(m_vecFootScreen.y - m_vecHeadScreen.y);
-		box.w = int(box.h / 2);
-		box.x = int(m_vecHeadScreen.x - box.w / 2);
-		box.y = int(m_vecHeadScreen.y);
+	Vector points[] = {
+		Vector(m_min.x, m_min.y, m_min.z),
+		Vector(m_min.x, m_max.y, m_min.z),
+		Vector(m_max.x, m_max.y, m_min.z),
+		Vector(m_max.x, m_min.y, m_min.z),
+		Vector(m_max.x, m_max.y, m_max.z),
+		Vector(m_min.x, m_max.y, m_max.z),
+		Vector(m_min.x, m_min.y, m_max.z),
+		Vector(m_max.x, m_min.y, m_max.z)
+	};
+
+	Vector pointsTransformed[8];
+	for (int i = 0; i < 8; i++) {
+		g_math.vector_transform_2(points[i], trans, pointsTransformed[i]);
 	}
 
-	return box;
 
+	if (!g_drawings.world_to_screen(pointsTransformed[3], flb) || !g_drawings.world_to_screen(pointsTransformed[5], brt)
+		|| !g_drawings.world_to_screen(pointsTransformed[0], blb) || !g_drawings.world_to_screen(pointsTransformed[4], frt)
+		|| !g_drawings.world_to_screen(pointsTransformed[2], frb) || !g_drawings.world_to_screen(pointsTransformed[1], brb)
+		|| !g_drawings.world_to_screen(pointsTransformed[6], blt) || !g_drawings.world_to_screen(pointsTransformed[7], flt))
+		return box;
+
+	Vector arr[] = { flb, brt, blb, frt, frb, brb, blt, flt };
+
+	// Init this shit
+	auto left = flb.x;
+	auto top = flb.y;
+	auto right = flb.x;
+	auto bottom = flb.y;
+
+	// Find the bounding corners for our box
+	for (int i = 1; i < 8; i++)
+	{
+		if (left > arr[i].x)
+			left = arr[i].x;
+		if (bottom < arr[i].y)
+			bottom = arr[i].y;
+		if (right < arr[i].x)
+			right = arr[i].x;
+		if (top > arr[i].y)
+			top = arr[i].y;
+	}
+
+	box.x = left;
+	box.y = top;
+	box.w = right - left;
+	box.h = bottom - top;
+
+	return box;
 }
 
 auto c_visuals::boundingbox(c_entity* entity, b_box box) -> void
